4aeeb6bb97a6bdb8819889b2bad17c05bed8decb
Cassandra Pamplona <cpamplona@sjcoe.net>
2021-04-21 13:50:37 -0700

    use of directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,23 @@ import { Component } from '@angular/core';
       Welcome to {{ title }}!
     </h1>
     <app-input-btn-unit></app-input-btn-unit>
+
+    <ul>
+      <li *ngFor="let item of todoList">
+        {{ item.title }}
+      </li>
+    </ul>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'todo-list';
+  todoList = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
 }
